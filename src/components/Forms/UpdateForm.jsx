f697b5730cf380770cab902bc0abfdb832d58e46
Elliot Kim <elliotkim916@gmail.com>
2020-06-04 21:06:52 -0700

    Added focus to UpdateForm

--- src/components/Forms/UpdateForm.jsx
+++ src/components/Forms/UpdateForm.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import { updateTask } from '../../api';
 
 const UpdateForm = ({ task, setShowUpdateModal, history }) => {
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    inputEl.current.focus();
+  }, []);
+
   return (
     <Formik
       initialValues={{ description: task.description, details: task.details, completed: task.completed }}
@@ -21,7 +27,7 @@ const UpdateForm = ({ task, setShowUpdateModal, history }) => {
       }}
     >
       <Form>
-        <Field name="description" type="text" placeholder="Enter description"/><br />
+        <Field name="description" type="text" placeholder="Enter description" innerRef={inputEl} /><br />
         <ErrorMessage name="description" /><br/>
         <Field name="details" type="text" placeholder="Enter details" /><br />
         <Field name="completed" type="text" placeholder="Task completed, true or false?"/><br />
