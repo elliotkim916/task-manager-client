8b9b041b7c5d6a5b8dad57f229514ded36dded5f
Elliot Kim <elliotkim916@gmail.com>
2020-07-24 17:45:00 -0700

    Added modals for when deletion or update of task is complete

--- src/components/Task/ViewTask.jsx
+++ src/components/Task/ViewTask.jsx
@@ -5,44 +5,93 @@ import UpdateForm from '../Forms/UpdateForm';
 
 const ViewTask = (props) => {
   const [task, setTask] = useState({});
+
   const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [showDeleteCompleteModal, setShowDeleteCompleteModal] = useState(false);
+
   const [showUpdateModal, setShowUpdateModal] = useState(false);
+  const [showUpdateCompleteModal, setShowUpdateCompleteModal] = useState(false);
 
   useEffect(() => {
-    getTask(props.match.params.id)
-      .then(data => {
-        setTask({...data});
-      });
+    getTask(props.match.params.id).then((data) => {
+      setTask({ ...data });
+    });
   }, [props]);
-  
-  let deleteModal, updateModal;
+
+  let deleteModal, deleteCompleteModal, updateModal, updateCompleteModal;
 
   if (showDeleteModal) {
-    deleteModal = <DeleteForm id={props.match.params.id} setShowDeleteModal={setShowDeleteModal} history={props.history} />
+    deleteModal = (
+      <DeleteForm
+        id={props.match.params.id}
+        setShowDeleteModal={setShowDeleteModal}
+        setShowDeleteCompleteModal={setShowDeleteCompleteModal}
+        history={props.history}
+      />
+    );
   } else {
     deleteModal = null;
   }
 
+  if (showDeleteCompleteModal) {
+    deleteCompleteModal = (
+      <div>
+        <h3>Your task was successfully deleted!</h3>
+        <button type="button" onClick={() => props.history.push('/dashboard')}>
+          Okay
+        </button>
+      </div>
+    );
+  } else {
+    deleteCompleteModal = null;
+  }
+
   if (showUpdateModal) {
-    updateModal = <UpdateForm task={task} setShowUpdateModal={setShowUpdateModal} history={props.history} />;
+    updateModal = (
+      <UpdateForm
+        task={task}
+        setShowUpdateModal={setShowUpdateModal}
+        setShowUpdateCompleteModal={setShowUpdateCompleteModal}
+        history={props.history}
+      />
+    );
   } else {
     updateModal = null;
+  }
+
+  if (showUpdateCompleteModal) {
+    updateCompleteModal = (
+      <div>
+        <h3>Your update is complete!</h3>
+        <button type="button" onClick={() => props.history.push('/dashboard')}>
+          Okay
+        </button>
+      </div>
+    );
+  } else {
+    updateCompleteModal = null;
   }
 
   const { completed, description, details, createdAt } = task;
 
   return (
     <div>
-      <button type="button" onClick={() => setShowDeleteModal(true)}>Delete</button>
+      <button type="button" onClick={() => setShowDeleteModal(true)}>
+        Delete
+      </button>
       <h1>{description}</h1>
       <p>{details}</p>
       <p>Completed : {String(completed)}</p>
       <p>{createdAt}</p>
 
-      <button type="button" onClick={() => setShowUpdateModal(true)}>Update Task</button>
+      <button type="button" onClick={() => setShowUpdateModal(true)}>
+        Update Task
+      </button>
 
       {updateModal}
+      {updateCompleteModal}
       {deleteModal}
+      {deleteCompleteModal}
     </div>
   );
 };
