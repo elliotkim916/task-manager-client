5cad51c21a0db6dfb762fcb4b58e5dc95d806818
Elliot Kim <elliotkim916@gmail.com>
2020-07-24 17:46:39 -0700

    Using a new Provider

--- src/App.js
+++ src/App.js
@@ -7,21 +7,61 @@ import SignupPage from './components/Auth/SignupPage';
 import Dashboard from './components/Dashboard/Dashboard';
 import ViewTask from './components/Task/ViewTask';
 import history from './history';
+// import { getTasks } from './api';
+// import TasksContext from './context/TasksContext';
+import { Provider as TasksProvider } from './context/TasksContext';
 
 class App extends React.Component {
+  // state = {
+  //   allTasks: [],
+  // };
+
+  // async componentDidMount() {
+  // const { allTasks } = this.state;
+  // if (allTasks.length === 0) {
+  //   const tasks = await getTasks();
+  //   this.setState({ allTasks: [...tasks] });
+  // }
+  // }
+
   render() {
+    // const { allTasks } = this.state;
+    // console.log(allTasks);
+
     return (
       <div className="App">
-        <Router history={history}>
-          <Route exact path="/" render={() => <LandingPage history={history} />} />
-          <Route exact path="/signup" render={() => <SignupPage history={history} />} />
-          <Route exact path="/login" render={() => <LoginPage history={history} />} />
-          <Route exact path="/dashboard" render={() => <Dashboard history={history} />} /> 
-          <Route exact path="/task/:id" render={(props) => <ViewTask history={history} {...props}/>} /> 
-        </Router>
+        <TasksProvider>
+          <Router history={history}>
+            <Route
+              exact
+              path="/"
+              render={() => <LandingPage history={history} />}
+            />
+            <Route
+              exact
+              path="/signup"
+              render={() => <SignupPage history={history} />}
+            />
+            <Route
+              exact
+              path="/login"
+              render={() => <LoginPage history={history} />}
+            />
+            <Route
+              exact
+              path="/dashboard"
+              render={() => <Dashboard history={history} />}
+            />
+            <Route
+              exact
+              path="/task/:id"
+              render={(props) => <ViewTask history={history} {...props} />}
+            />
+          </Router>
+        </TasksProvider>
       </div>
     );
   }
-};
+}
 
 export default App;
